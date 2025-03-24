fcd0fa5a10e712d61525b45a6fe2ddf936388b05
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-03-24 10:54:04 +0100

    New changes.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -101,7 +101,7 @@ export default function Navbar() {
                         )
                     )}
                     <a
-                        href="#contact"
+                        href="/contact"
                         className="inline-flex text-white border-2 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-full text-lg"
                     >
                         Contact
