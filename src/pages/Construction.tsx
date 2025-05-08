ae27fe462864e3eb53d4e7deac99b37ae8e9c64b
Kamil Markowski <kamil0markowski@gmail.com>
2025-05-08 15:34:20 +0200

    construction styling modified

--- src/pages/Construction.tsx
+++ src/pages/Construction.tsx
@@ -3,8 +3,9 @@ import logo from '../styles/images/logo.jpg'
 export default function Construction() {
   return (
     <div className="flex flex-col justify-center items-center h-dvh w-full">
-        <img src={logo} alt="Logo" className="w-1/5 h-auto mb-10 animate-wiggle" />
+        <img src={logo} alt="Logo" className="w-[350px] h-auto mb-10 animate-wiggle" />
         <div>To tutaj niebawem będzie nasza strona internetowa</div>
+        <div className="h-[2px] w-[200px] bg-blue-500 my-5"></div>
         <div className="">C'est ici que notre site web sera bientôt disponible</div>
     </div>
   )
