4413baf3679423d4ab0fff950ef2fd0566bca6dc
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-05-27 13:11:48 +0200

    Second part of the Contact Section.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -6,6 +6,7 @@ export default function ContactForm() {
     const [formData, setFormData] = useState<FormData>({
         name: "",
         email: "",
+        phone: "",
         title: "",
         message: "",
     });
@@ -19,12 +20,15 @@ export default function ContactForm() {
         const titleInput = (e.currentTarget as HTMLElement).querySelector<HTMLInputElement>('#title')!.value;
         const emailInput = (e.currentTarget as HTMLElement).querySelector<HTMLInputElement>('#email')!.value;
         const messageInput = (e.currentTarget as HTMLElement).querySelector<HTMLTextAreaElement>('#message')!.value;
+        const phoneInput = (e.currentTarget as HTMLElement).querySelector<HTMLTextAreaElement>('#phone')!.value;
+
 
         const form = {
             name: nameInput,
             email: emailInput,
             title: titleInput,
             message: messageInput,
+            phone: phoneInput,
         };
 
         setFormData(form);
@@ -48,6 +52,7 @@ export default function ContactForm() {
                     email: "",
                     title: "",
                     message: "",
+                    phone: "",
                 });
                 (e.target as HTMLFormElement).reset();
             } else {
@@ -64,7 +69,8 @@ export default function ContactForm() {
             <div className="bg-polka-bl-green" />
             <div className="absolute top-0 right-0 w-24 h-24 bg-blue-600 rounded-tr-full z-0" />
             <div className="bg-polka-br-green top-1/6 right-0" />
-            <section id="contact" className="relative z-10 bg-white shadow-xl rounded-xl p-8 max-w-md w-full">
+            <section id="contact" className="relative z-10 bg-white shadow-xl rounded-xl p-8 max-w-full w-lg">
+
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <Input
                         type="text"
@@ -79,18 +85,25 @@ export default function ContactForm() {
                     />
                     <Input
                         type="text"
+                        id="title"
+                        placeholder="Title"
+                    />
+                    <Input
+                        type="text"
                         id="phone"
                         placeholder="Your Phone"
 
                     />
-                    <textarea
+                    <Input
+                        type="textarea"
                         id="message"
                         placeholder="Your Message"
-                        className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 h-32 resize-none"
                     />
                     <button
                         type="submit"
-                        className="w-full bg-blue-600 text-white py-3 rounded-md font-medium hover:bg-blue-700 transition"
+                        // className="w-full bg-blue-600 text-white py-4 rounded-md font-medium hover:bg-blue-700 transition mt-4"
+                        className="button button-blue size-fit px-6 py-3 text-xl font-light w-full"
+
                     >
                         Send Message
                     </button>
