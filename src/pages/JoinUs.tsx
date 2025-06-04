dbcb65d13f7691860eb2c6787f4fb4f4dca98fbf
Kamil Markowski <kamil0markowski@gmail.com>
2025-06-04 19:42:19 +0200

    join us page updated

--- src/pages/JoinUs.tsx
+++ src/pages/JoinUs.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import useFetch from '../hooks/useFetch';
+import { BlocksRenderer } from '@strapi/blocks-react-renderer';
+
 
 export default function JoinUs() {
+
+    const { data, loading, error } = useFetch('join-us-page');
+    console.log(data);
+
+
   return (
     <div className="w-3/5">
         <section className="flex flex-col mt-20">
             <div className="flex flex-col gap-8">
                 <div className="flex flex-col gap-2">
                     <h4>How to join us</h4>
-                    <p className="">Explain here what your future members need to do to join our association. Can be a long description or not, it’s up to you to decide. Just make sure it’s transparent, not too long, not too detailed, with just enough informations for people to choose you.</p>
+                    { <BlocksRenderer content={data.Content}  />}
                 </div>
                 <div className="flex flex-col gap-2">
-                    <h4>Why join us</h4>
+                    <h4>Dlaczego do nas dołączyć?</h4>
                     <p className="">Explain here all avantages your future member will have access to, if they join you. Can be a long description or not, it’s up to you to decide. Just make sure it’s transparent, not too long, not too detailed, with just enough informations for people to choose you.</p>
                 </div>
                 <div className="flex flex-col gap-2">
