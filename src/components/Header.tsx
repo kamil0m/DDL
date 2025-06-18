74e966b832183f3a4684d9d0ce5c808fc1210db0
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-06-18 16:18:51 +0200

    Changing the file Header for translations.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Logo from "./Logo";
 import NavBar from "./NavBar";
 import { NavLink } from "react-router-dom";
 import SearchBar from "./SearchBar";
+import { useTranslation } from "react-i18next";
 
 export default function Header() {
   const scrollToContact = () => {
@@ -11,6 +12,9 @@ export default function Header() {
       contactElement.scrollIntoView({ behavior: "smooth" });
     }
   };
+
+  const { t } = useTranslation();
+
   return (
     <header className="container flex flex-row justify-between items-center w-full py-4 text-xl">
 
@@ -22,8 +26,8 @@ export default function Header() {
 
       <div className="flex flex-row justify-end items-center gap-4">
         <LanguageToggle />
-        <button onClick={scrollToContact} className="button button-transparent hover:button-accent px-8 py-2">Kontakt</button>
-        <NavLink to="/joinus" className="button button-accent hover:button-transparent px-8 py-2">Dołącz do nas</NavLink>
+        <button onClick={scrollToContact} className="button button-transparent hover:button-accent px-8 py-2">{t("header.contact")}</button>
+        <NavLink to="/joinus" className="button button-accent hover:button-transparent px-8 py-2">{t("header.joinus")}</NavLink>
       </div>
 
     </header>
