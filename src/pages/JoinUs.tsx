e983b5c599c595c3b6df18226d62212a854daa59
Kamil Markowski <kamil0markowski@gmail.com>
2025-06-23 19:53:35 +0200

    helloasso issue solved

--- src/pages/JoinUs.tsx
+++ src/pages/JoinUs.tsx
@@ -6,7 +6,7 @@ import RenderRichText from '../components/RenderRichText.tsx';
 export default function JoinUs() {
 
     const { data, loading, error } = useFetch('join-us-page');
-    console.log(data);
+    // console.log(data);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error!</p>;
@@ -31,7 +31,7 @@ export default function JoinUs() {
                 </div>
                 <div className="flex flex-col gap-2">
                     <h4>HelloAsso</h4>
-                    <p className="">Explain here what HelloAsso is here, and also explain how your members’ and future members’ donations will be used. Can be a long description or not, it’s up to you to decide. Just make sure it’s transparent, not too long, not too detailed, with just enough informations for people to choose you.</p>
+                    { <RenderRichText content={data.Hello_Asso}  />}
                     <a href="https://www.helloasso.com/associations/dzien-dobry-lille/adhesions/adhesion" target="_blank" className="button button-accent w-[250px] py-3 mt-10 self-center animate-bounce text-3xl">☞ HelloAsso</a>
                 </div>
             </div>
