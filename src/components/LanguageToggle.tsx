f4083c0ce876be0f38e366144e0a43869569fb5d
Kamil Markowski <kamil0markowski@gmail.com>
2025-06-27 13:35:46 +0200

    header styling improved

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -25,37 +25,13 @@ export default function LanguageToggle({ onLanguageChange }: LanguageToggleProps
         <label
             onClick={switchLanguage}
             id="toggle"
-            className={`flex flex-row justify-between items-center gap-1.5 h-[2.5em] bg-accent rounded-full text-sm px-1 py-3 cursor-pointer ${
+            className={`relative flex flex-row justify-between items-center gap-1.5 h-[2.5em] bg-accent rounded-full text-sm px-1 py-3 cursor-pointer ${
                 isLanguageChanging ? 'opacity-50 cursor-not-allowed' : ''
             }`}
         >
-            <span className={`switch ${i18n.language === "pl" ? "switch-on" : ""}`}>PL</span>
-            <span className={`switch ${i18n.language === "fr" ? "switch-on" : ""}`}>FR</span>
+            <div className={`z-1 absolute flex justify-center items-center left-1 top-1 h-8/10 w-[2.2em] rounded-full bg-white transition-transform duration-300 ease-in-out ${i18n.language === "fr" ? "translate-x-0" : "translate-x-[2.3em]"}`}></div>
+            <span className={`switch ${i18n.language === "fr" ? "text-accent" : ""} transition-colors duration-300 ease-in-out`}>FR</span>
+            <span className={`switch ${i18n.language === "pl" ? "text-accent" : ""} transition-colors duration-300 ease-in-out`}>PL</span>
         </label>
-    );
-
-
-
-    // Version 1
-    // export default function LanguageToggle() {
-
-    //     const switchLanguage = (e) => {
-    //         const toggle: HTMLInputElement = e.currentTarget;
-    //         const languageSwitches = toggle.querySelectorAll('.switch');
-    //         languageSwitches.forEach((languageSwitch) => {
-    //             languageSwitch.classList.toggle('switch-on');
-    //         });
-    //         // ToDo : Add animated transition in CSS
-    //     }
-
-    //     return (
-    //         <label onClick={switchLanguage} id="toggle" className="flex flex-row justify-between items-center gap-1.5 h-[2.5em] bg-accent rounded-full text-sm px-1 py-3 cursor-pointer">
-    //             {/* ToDo : Integrate the checkbox to the multilingual handler */}
-    //             {/* <input id="language-toggle" className="hidden" type="checkbox" /> */}
-    //             <span className="switch switch-on">PL</span>
-    //             <span className="switch">FR</span>
-    //         </label>
-    //     )
-
-
+    )
 }
