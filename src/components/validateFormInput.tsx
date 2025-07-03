bbb564bdce9a83199ff5c19745524a50b9ce1dc2
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-07-03 18:15:00 +0200

    Nonrequired phone in contact form.

--- src/components/validateFormInput.tsx
+++ src/components/validateFormInput.tsx
@@ -1,20 +1,19 @@
-export default function validateFormInput(input: string, type: string): string {
+import { TFunction } from "i18next";
 
+export default function validateFormInput(input: string, type: string, t: TFunction): string {
     let errorMessage = '';
 
     if (type === 'email') {
-
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
         if (!emailRegex.test(input)) {
-            errorMessage = 'Niepoprawny adres e-mail';
+            errorMessage = t("contact.form.validation.errorMail");
         }
     }
 
-    if (input === '') {
-        errorMessage = 'Pole jest wymagane';
+    if (input === '' && type !== 'tel') {
+        errorMessage = t("contact.form.validation.errorBox");
+    }
 
-    } 
-
-  return errorMessage
+    return errorMessage;
 }
