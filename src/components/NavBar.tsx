5fbc6fcdc49565eb0e573ce495b1b797310ca838
Kamil Markowski <kamil0markowski@gmail.com>
2025-08-01 14:16:46 +0200

    header navigation updated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,7 @@ export default function NavBar() {
 
             <NavLink to="/" className="flex justify-center xw-45 text-accent hover:text-red">{t("nav.home")}</NavLink>
             <NavLink to="/events" className="flex justify-center xw-45 text-accent hover:text-red">{t("nav.events")}</NavLink>
-            <NavLink to="/new" className="flex justify-center Xw-45 text-accent hover:text-red">{t("nav.news")}</NavLink>
+            <NavLink to="/news" className="flex justify-center Xw-45 text-accent hover:text-red">{t("nav.news")}</NavLink>
             <NavLink to="/about" className="flex justify-center xw-45 text-accent hover:text-red">{t("nav.about")}</NavLink>
 
         </nav>
