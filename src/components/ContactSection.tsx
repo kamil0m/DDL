bc59e647b579581f73222717fcd8ad364a39f9d0
Kamil Korzeniowski <kamil.korzeniowski.fr@gmail.com>
2025-08-02 13:20:01 +0200

    Change to CSS classes of ContactSection component.

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -15,23 +15,23 @@ export default function ContactSection({data}: {data?: any}) {
                 <div className="flex flex-col gap-6">
 
                     {/* Facebook group */}
-                    <div className="flex items-center gap-4">
-                        <a href="https://www.facebook.com/groups/1403266290513975" target="_blank" className="bg-zinc-200 p-5 rounded-lg">
+                    <div className="fb-box">
+                        <a href="https://www.facebook.com/groups/1403266290513975" target="_blank" className="fb-icon-wrapper">
                             <BiLogoFacebookCircle className="text-blue-600 text-4xl" />
                         </a>
                         <div>
-                            <a href="https://www.facebook.com/groups/1403266290513975" target="_blank" className="font-semibold underline text-black text-xl mt-0">{t("contact.facebookgroup")}</a>
+                            <a href="https://www.facebook.com/groups/1403266290513975" target="_blank" className="fb-title">{t("contact.facebookgroup")}</a>
                             <p className="mt-0">{t("contact.facebookgroupjoin")}</p>
                         </div>
                     </div>
 
                     {/* Facebook page */}
-                    <div className="flex items-center gap-4">
-                        <a href="https://www.facebook.com/profile.php?id=61559848594019" target="_blank" className="bg-zinc-200 p-5 rounded-lg">
+                    <div className="fb-box">
+                        <a href="https://www.facebook.com/profile.php?id=61559848594019" target="_blank" className="fb-icon-wrapper">
                             <BiLogoFacebookCircle className="text-blue-600 text-4xl" />
                         </a>
                         <div>
-                            <a href="https://www.facebook.com/profile.php?id=61559848594019" target="_blank" className="font-semibold underline text-black text-xl mt-0">{t("contact.facebookpage")}</a>
+                            <a href="https://www.facebook.com/profile.php?id=61559848594019" target="_blank" className="fb-title">{t("contact.facebookpage")}</a>
                             <p className="mt-0">{t("contact.facebookpageinfo")}</p>
                         </div>
                     </div>
