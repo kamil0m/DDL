d971cc7c3e58c5cec99c59efda467a248f4d34af
Kamil Korzeniowski <kamil.korzeniowski.fr@gmail.com>
2025-08-18 23:41:54 +0200

    Changes to People and NewsItem files.

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { HiOutlineNewspaper } from "react-icons/hi2";
 import { isSoon, isNew, publishedDaysAgo } from '../components/FilteringMethods';
